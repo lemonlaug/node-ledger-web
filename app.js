312b6202b2921136c792bc219b7388a403630cff
Thomas R. Koll <tomk32@tomk32.de>
2017-04-21 10:06:09 +0200

    WIP: Upgrade to express 4 and use an updated node-ledger-rest

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var _ = require('lodash'),
     express = require('express'),
+    //favicon = require('serve-favicon'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    logger = require('morgan'),
+    errorHandler = require('errorhandler'),
     home = require('./routes/home'),
     http = require('http'),
     path = require('path'),
@@ -16,42 +21,39 @@ try {
 
 var app = express();
 
-app.configure(function() {
-  var port = parseInt(process.env.PORT || 3000, 10);
-  
-  app.set('port', port);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'html');
-  app.engine('html', engine);
-  
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-  
-  var proxy = httpProxy.createProxyServer();
-  
-  // Example ledger .dat file from the appendix of the Ledger 3 manual
-  var ledgerRest = new LedgerRest(config);
-  
-  var ledgerRestPort = port + 1;
-  ledgerRest.listen(ledgerRestPort, function() {
-    console.log('Ledger REST server listening on port ' + ledgerRestPort);
-  });
-  
-  // Proxy API requests to the ledger REST service
-  app.use('/api', function (req, res) {
-    proxy.web(req, res, { target: {
-      host: 'localhost',
-      port: port + 1
-    }});
-  });
+var port = process.env.PORT || 3000
+app.set('port', port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', engine);
+
+//app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ resave: true, saveUninitialized: true, secret: process.env.SESSION_SECRET || 'pleasechangeme'}))
+
+var proxy = httpProxy.createProxyServer();
+
+// Example ledger .dat file from the appendix of the Ledger 3 manual
+var ledgerRest = new LedgerRest(config);
+
+var ledgerRestPort = port + 1;
+ledgerRest.listen(ledgerRestPort, function() {
+  console.log('Ledger REST server listening on port ' + ledgerRestPort);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+// Proxy API requests to the ledger REST service
+app.use('/api', function (req, res) {
+  proxy.web(req, res, { target: {
+    host: 'localhost',
+    port: port + 1
+  }});
 });
+
+if (app.get('env') == 'development') {
+  app.use(errorHandler());
+}
 
 var routes = [
   '/',
@@ -65,6 +67,7 @@ _.each(routes, function(route) {
   app.get(route, home.index);
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
